634bb60445c6f4b5d753837ade00bb5cf49263b4
itspoma <itspoma@gmail.com>
2016-06-13 21:32:50 +0300

    added fake data, integrated with app

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1,44 +1,15 @@
 var app = angular.module('ETicketsApp', ['ngMaterial']);
 
-var ticketsData = [
-  {
-    title: 'first',
-    image: 'http://placekitten.com/100/100'
-  },
-  {
-    title: 'second',
-    image: 'http://placekitten.com/100/101'
-  },
-  {
-    title: 'third',
-    image: 'http://placekitten.com/100/102'
-  },
-  {
-    title: 'third',
-    image: 'http://placekitten.com/100/102'
-  },
-  {
-    title: 'third',
-    image: 'http://placekitten.com/100/102'
-  },
-  {
-    title: 'third',
-    image: 'http://placekitten.com/100/102'
-  },
-  {
-    title: 'third',
-    image: 'http://placekitten.com/100/102'
-  },
-  {
-    title: 'third',
-    image: 'http://placekitten.com/100/102'
-  },
-  {
-    title: 'last',
-    image: 'http://placekitten.com/100/103'
-  }
-];
+app.service('tickets', function ($http) {
+  this.getAll = function () {
+    return $http.get('/mocks/tickets.json');
+  };
+});
 
-app.controller('listCtrl', function ($scope) {
-  $scope.tickets = ticketsData;
+app.controller('listCtrl', function ($scope, tickets) {
+  $scope.tickets = [];
+
+  tickets.getAll().then(function (response) {
+    $scope.tickets = response.data;
+  });
 });
